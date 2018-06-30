424ee5940757c4e8d7f61501ef4ba02db9c7c053
bewarang <dogak153@yahoo.com>
2018-06-30 13:59:18 +0100

    Completed Project

--- designs.js
+++ designs.js
@@ -1,10 +1,32 @@
 // Select color input
 // Select size input
-
+var color, height, width, cell;
 // When size is submitted by the user, call makeGrid()
+$("#sizePicker").submit(function(e){
+  e.preventDefault();
+  color = $('#colorPicker').val();
+  height =$('#inputHeight').val();
+  width = $('#inputWeight').val();
+  makeGrid();
+});
+
+$('#colorPicker').change(function(){
+  color = $('#colorPicker').val();
+});
 
 function makeGrid() {
-
+  for(var i=0; i<height; i++){
+    $('#pixelCanvas').append('<tr></tr>');
+  }
+  var rows = $('#pixelCanvas').children('tr');
+  rows.each(function(){
+    for(var j=0; j<width; j++){
+      $(this).append('<td></td>');
+    }
+  cell = $(this).find('td');
+    cell.click(function(){
+      $(this).attr('bgcolor',color);
+    });
+  });
 // Your code goes here!
-
 }
